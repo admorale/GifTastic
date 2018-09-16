31e2596d6a7bdcad3b7915f18aac3afa8e013790
admorale <ademar.morales@yahoo.com>
2018-09-16 02:03:14 -0500

    minor bugs fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
-var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"]
-
+// Array with the initial buttons
+var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar-glider", "chinchilla", "hedgehog", "hermit-crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup-pig", "serval", "salamander", "frog"];
+// Initial fucntion to populate the first buttons
 function initialLoad(){
+$("#topics").empty();
 for (i=0; i < animals.length; i++) {
     var topicName = animals[i];
     $("#topics").append("<button class='topicBtn' id="+topicName+">"+topicName+"</button>")
@@ -8,34 +10,79 @@ for (i=0; i < animals.length; i++) {
 }
 initialLoad();
 
-$("#searchAnimal").on("click", function(){
-    var topicValue = $("#searchTopic").val();
-    $("#topics").append("<button class='topicBtn' id="+topicValue+">"+topicValue+"</button>");
-    $("#searchTopic").val("");
-})
-
-$(".topicBtn").on("click", function(event) {
-    //console.log(event.target.id);
+// Generating GIFs from animal buttons
+$(".topicBtn").on("click", function(event) {    
+    function addEventToBtn() {
+    $("#topicGif").empty();
     var topicValue = event.target.id;
-    //console.log(topicValue);
     var URL = "http://api.giphy.com/v1/gifs/search?q="+topicValue+"&limit=10&api_key=ePH8hUjeUvUmm5FngErya18PPS3yvnKZ"
     $.ajax({
         url: URL,
         method: "GET",
-        //data: {"api-key":"688ebf136fc34c92aa7ab402ce7123df", 'q': searchTerm, 'begin_date': startDate, 'end_date': endDate,"sort":"newest"}
       }).then(function(resp) {
-        //console.log(resp);
         responses=resp.data;
-        console.log(responses);
         var animalName = "GIF"+topicValue;
-        //var animalNameRepo = animalName+i
-        console.log(animalName);
         for (i=0; i<responses.length;i++){
             $("#topicGif").append("<div class ='gifRepo' id='"+animalName+i+"'></div>");
-            $("#"+animalName+i).append("<p> Rating: "+responses[i].rating)+"</p>";
-            $("#"+animalName+i).append("<img src="+responses[i].images.fixed_width_still.url+" data-still='"+responses[i].images.fixed_width_still.url+"' data-animate='"+responses[i].images.fixed_width.url+"' data-state='still' class='gif'>");
+            $("#"+animalName+i).append("<p> Rating: "+responses[i].rating+"</p>");
+            $("#"+animalName+i).append("<img src="+responses[i].images.fixed_height_still.url+" data-still='"+responses[i].images.fixed_height_still.url+"' data-animate='"+responses[i].images.fixed_height.url+"' data-state='still' class='gif'>");
         }
+        // Adding event listener to the dinamically created GIFs 
+        $(".gif").on("click", function(event) {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+             } else {
+               $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+          // End of section - Adding event listener to new dinamically generated GIFs 
+    }
+      )}
+      addEventToBtn();
 })
-})
+// Create new animal buttons
+$("#searchAnimal").on("click", function(){
+    var topicValue = $("#searchTopic").val();
+    $("#topics").append("<button class='topicBtn' id="+topicValue+">"+topicValue+"</button>");
+    $("#searchTopic").val("");
+    animals.push(topicValue);
+    // Adding click event listener to the new buttons
+    $(".topicBtn").on("click", function(event) {
+        function addEventToBtn() {
+        $("#topicGif").empty();
+        var topicValue = event.target.id;
+        var URL = "http://api.giphy.com/v1/gifs/search?q="+topicValue+"&limit=10&api_key=ePH8hUjeUvUmm5FngErya18PPS3yvnKZ"
+        $.ajax({
+            url: URL,
+            method: "GET",
+            //data: {"api-key":"688ebf136fc34c92aa7ab402ce7123df", 'q': searchTerm, 'begin_date': startDate, 'end_date': endDate,"sort":"newest"}
+          }).then(function(resp) {
+            responses=resp.data;
+            var animalName = "GIF"+topicValue;
+            for (i=0; i<responses.length;i++){
+                $("#topicGif").append("<div class ='gifRepo' id='"+animalName+i+"'></div>");
+                $("#"+animalName+i).append("<p> Rating: "+responses[i].rating)+"</p>";
+                $("#"+animalName+i).append("<img src="+responses[i].images.fixed_width_still.url+" data-still='"+responses[i].images.fixed_width_still.url+"' data-animate='"+responses[i].images.fixed_width.url+"' data-state='still' class='gif'>");
+            }
+            // Adding event listener to the dinamically created GIFs 
+            $(".gif").on("click", function(event) {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            }
+            });
+            // End of section - Adding event listener to new dinamically generated GIFs   
+        }
+          )}
+          addEventToBtn();
+    })
 
+})
 
